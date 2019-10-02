ede59938dd99e6d0e62dd05f19a858fd70c22f01
Orta Therox <orta.therox@gmail.com>
2019-10-02 14:03:23 -0400

    Some theming tweaks

--- public/main-1.js
+++ public/main-1.js
@@ -693,7 +693,7 @@ async function main() {
           }
 
           const sectionSubtitle = document.createElement("p")
-          sectionSubtitle.textContent = s.subtitle
+          sectionSubtitle.innerHTML = s.subtitle
           sectionSubtitle.style.width = "100%"
           sectionContent.appendChild(sectionSubtitle)
 
@@ -967,6 +967,9 @@ console.log(message);
   const outputDefault = window.CONFIG.useJavaScript ? "// Using JavaScript, no compilation needed." : ""
   State.outputModel = monaco.editor.createModel(outputDefault, "javascript", monaco.Uri.file("output.js"));
 
+  monaco.editor.defineTheme("sandbox", sandboxTheme)
+  monaco.editor.setTheme("sandbox")
+
   inputEditor = monaco.editor.create(
     document.getElementById("input"),
     Object.assign({
@@ -1175,5 +1178,76 @@ function objectToQueryParams(obj) {
 }
 
 
+// Color theme:
 
+const blue = "3771EF";
+const darkerBlue = "1142AF";
+const darkestBlue = "09235D";
 
+const yellow = "F3DF51";
+const darkYellow = "AEA811";
+const darkerYellow = "65610A";
+
+const grey = "84864d";
+const green = "12CD0E";
+const greenDark = "10990D";
+const greenLight = "54F351";
+
+const sandboxTheme = {
+  base: "vs",
+  inherit: true,
+  rules: [
+    { token: "", foreground: "000000", background: "fffffe" },
+    { token: "invalid", foreground: "cd3131" },
+    { token: "emphasis", fontStyle: "italic" },
+    { token: "strong", fontStyle: "bold" },
+
+    { token: "variable", foreground: "11bb11" },
+    { token: "variable.predefined", foreground: "4864AA" },
+    { token: "constant", foreground: "44ee11" },
+    { token: "comment", foreground: grey },
+    { token: "number", foreground: greenDark },
+    { token: "number.hex", foreground: "3030c0" },
+    { token: "regexp", foreground: greenLight },
+    { token: "annotation", foreground: "808080" },
+    { token: "type", foreground: darkerBlue },
+
+    { token: "delimiter", foreground: "000000" },
+    { token: "delimiter.html", foreground: "383838" },
+    { token: "delimiter.xml", foreground: "0000FF" },
+
+    { token: "tag", foreground: "800000" },
+    { token: "tag.id.pug", foreground: "4F76AC" },
+    { token: "tag.class.pug", foreground: "4F76AC" },
+    { token: "meta.scss", foreground: "800000" },
+    { token: "metatag", foreground: "e00000" },
+    { token: "metatag.content.html", foreground: "FF0000" },
+    { token: "metatag.html", foreground: "808080" },
+    { token: "metatag.xml", foreground: "808080" },
+    { token: "metatag.php", fontStyle: "bold" },
+
+    { token: "key", foreground: "863B00" },
+    { token: "string.key.json", foreground: "A31515" },
+    { token: "string.value.json", foreground: "0451A5" },
+
+    { token: "attribute.name", foreground: "FFFF00" },
+    { token: "attribute.value", foreground: "0451A5" },
+    { token: "attribute.value.number", foreground: "09885A" },
+    { token: "attribute.value.unit", foreground: "09885A" },
+    { token: "attribute.value.html", foreground: "0000FF" },
+    { token: "attribute.value.xml", foreground: "0000FF" },
+
+    { token: "string", foreground: greenDark },
+
+    { token: "keyword", foreground: blue },
+    { token: "keyword.json", foreground: "0451A5" }
+  ],
+  colors: {
+    editorBackground: "#F6F6F6",
+    editorForeground: "#000000",
+    editorInactiveSelection: "#E5EBF1",
+    editorIndentGuides: "#D3D3D3",
+    editorActiveIndentGuides: "#939393",
+    editorSelectionHighlight: "#ADD6FF4D"
+  }
+};
